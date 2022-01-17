12d609d74b0c2610b6891fe288c89b777852f69d
Nancy Piedra <nancy_piedra@yahoo.com>
2022-01-17 11:36:14 +0000

    remove token

--- src/services/memoryStorage.js
+++ src/services/memoryStorage.js
@@ -1,7 +1,7 @@
 const MemoryStorage = (function(){
 
     var _service;
-    var __accessToken = 'd74j7mkxe9gym1hs156exm3i4';
+    var __accessToken = null;
     var __refreshToken = null;
     var __ccAccessToken = null;
     var __ccRefreshToken = null;
